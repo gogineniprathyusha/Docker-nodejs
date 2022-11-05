faf573b405e9c9a65e76f7a6725b31e44b7daa79
gogineniprathyusha <104327062+gogineniprathyusha@users.noreply.github.com>
2022-11-05 10:46:53 -0400

    dockerjs

--- Ec2.tf
+++ Ec2.tf
@@ -1,11 +1,10 @@
-resource "aws_instance" "dockerimage" {
+resource "aws_instance" "dockerimage123" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name = "Devops"
-  subnet_id = aws_subnet.docker-Subnet1.id
-  associate_public_ip_address = true
   user_data = file("dockerinstall.sh")
+  security_groups = [ "launch-wizard-3" ]
   tags = {
-    Name = "dockerimage"
+    Name = "dockerimage123"
 }
 }
--- main.tf
+++ main.tf
@@ -12,22 +12,3 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
-resource "aws_vpc" "docker-VPC"{
-    cidr_block = "10.0.0.0/16"
-
-    tags = {
-        Name = "docker-VPC"
-    }
-
-}
-
-# Create Subnet
-
-resource "aws_subnet" "docker-Subnet1" {
-    vpc_id = aws_vpc.docker-VPC.id
-    cidr_block = "10.0.1.0/24"
-
-    tags = {
-        Name = "docker-Subnet1"
-    }
-}
