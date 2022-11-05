556551fca80ddf329c46a0787ddd1c28f2837f0c
gogineniprathyusha <104327062+gogineniprathyusha@users.noreply.github.com>
2022-11-05 09:00:51 -0400

    dockerjs

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -7,3 +7,7 @@ chkconfig docker on
 yum install -y git
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/gogineniprathyusha/Docker-nodejs.git
+cd /Docker-nodejs/
+docker build -t node .
+docker run -t -d -p 3030:3030 node
